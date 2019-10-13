a6c6eb96b25e25bfed1ca7541c1814a1a18c2fc0
Jacob Ćwikowski <kcwikowski007@yahoo.com>
2019-10-13 22:51:29 +0200

    First Login configuration
    1) Added Login form
    2) Configured security
    3) Configured authenticator

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,17 +16,35 @@ class LoginType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class, [
+            ->add('email', EmailType::class, [
                 'required' => true,
-            ])
-            ->add('lastName', TextType::class, [
-                'required' => true,
-            ])
-            ->add('email', TextType::class, [
-                'required' => true,
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control',
+                    'autofocus' => true
+                ]
             ])
             ->add('password', PasswordType::class, [
                 'required' => true,
+                'label' => 'Password',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('checkbox', CheckboxType::class, [
+                'label' => 'Remember me',
+                'required' => false,
+                'data' => true,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-check-input',
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Sign in',
+                'attr' => [
+                    'class' => 'btn btn-login'
+                ]
             ])
 
         ;
---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use App\Repository\UserRepository;
+use Symfony\Component\Routing\RouterInterface;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+
+    private $userRepository;
+    private $router;
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'app_login'&& $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        //this is called immediately when supports returns true
+        //dump($request->request->all());die;
+        $postData = $request->request->get('login');
+        $email = $postData['email'];
+        $pass = $postData['password'];
+        return [
+            'email' => $email,
+            'password' => $pass
+        ];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        if($user==null) return false;
+        else
+        {
+            if($user->getPassword()==$credentials['password'])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return new RedirectResponse($this->router->generate('home'));
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->router->generate('app_login');
+    }
+}
