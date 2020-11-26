aae606ffefafae2aaeebb10146273ab3ade25046
martamazurkiewicz <marta@mazurkiewi.cz>
2020-11-26 12:56:06 +0100

    Career controller and view

--- src/Repository/CareerRepository.php
+++ src/Repository/CareerRepository.php
@@ -18,33 +18,4 @@ class CareerRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Career::class);
     }
-
-    // /**
-    //  * @return Career[] Returns an array of Career objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Career
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
