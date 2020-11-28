968f4e858eb3d4724e54e0f92f58196248fc77fd
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2020-11-28 19:59:15 +0100

    Refactoring OfferController - split displayOfferList into more functions

--- src/Controller/Offer/OfferController.php
+++ src/Controller/Offer/OfferController.php
@@ -4,11 +4,13 @@
 namespace App\Controller\Offer;
 
 use App\Entity\BookingOffer;
+use App\Entity\BookingOfferType;
 use App\Entity\Destination;
 use App\Form\BookingOfferFiltersType;
 use App\Service\BookingOfferService;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,28 +31,9 @@ class OfferController extends AbstractController
     {
         $bookingOffer = new BookingOffer();
         if($request->query->get('booking_offer_search')) {
-            $requestParams = $request->query->get('booking_offer_search');
-            $departureSpot = $requestParams['departureSpot'] ?? null;
-            $destination = $requestParams['destination'] ?? null;
-            $departureDate = $requestParams['departureDate'] ?? null;
-            $comebackDate = $requestParams['comebackDate'] ?? null;
-            $offers = $offerService->findOffers($departureSpot,
-                $destination,
-                $departureDate,
-                $comebackDate);
-            $bookingOffer->setDepartureSpot($departureSpot);
-            $bookingOffer->setDestination($this->getDoctrine()->getRepository(Destination::class)->find($destination));
-            if($departureDate!=null) {
-                $date = explode('/',$departureDate);
-                $date = $date[2] . '/' . $date[1] . '/' . $date[0];
-                $bookingOffer->setDepartureDate(new \DateTime($date));
-            }
-            if($comebackDate!=null) {
-                $date = explode('/',$comebackDate);
-                $date = $date[2] . '/' . $date[1] . '/' . $date[0];
-                $bookingOffer->setComebackDate(new \DateTime($date));
-            }
-
+            $offers = $this->getOffersBasedOnRequestQuery($offerService, $request, $bookingOffer);
+        } elseif ($request->query->get('offerType')) {
+            $offers = $this->getOffersBasedOnOfferType($offerService, $request->query->get('offerType'));
         } else {
             $offers = $offerService->findOffers();
         }
@@ -58,20 +41,8 @@ class OfferController extends AbstractController
             'method' => 'GET'
         ]);
         $filtersForm->handleRequest($request);
-
         if($filtersForm->isSubmitted() && $filtersForm->isValid()) {
-            $priceMin = $filtersForm->get('priceMin')->getData();
-            $priceMax = $filtersForm->get('priceMax')->getData();
-            $offerTypes = $filtersForm->get('offerTypes')->getData();
-            $offers = $offerService->findOffers(
-                $bookingOffer->getDepartureSpot(),
-                $bookingOffer->getDestination(),
-                $bookingOffer->getDepartureDate(),
-                $bookingOffer->getComebackDate(),
-                $priceMin,
-                $priceMax,
-                $offerTypes
-            );
+            $offers = $this->getOffersBasedOnFormSubmission($offerService, $filtersForm, $bookingOffer);
         }
         return $this->render('offer/browser.html.twig', [
             'offers' => $offers,
@@ -91,5 +62,60 @@ class OfferController extends AbstractController
         return $this->render('offer/single_offer.html.twig', [
             'offer' => $offer
         ]);
+    }
+
+    private function getWellFormattedDate(string $date)
+    {
+        $reformatted = explode('/',$date);
+        $reformatted = $reformatted[2] . '/' . $reformatted[1] . '/' . $reformatted[0];
+        return new \DateTime($reformatted);
+    }
+
+    private function getOffersBasedOnFormSubmission(BookingOfferService $offerService, FormInterface $filtersForm, BookingOffer $bookingOffer)
+    {
+        $priceMin = $filtersForm->get('priceMin')->getData();
+        $priceMax = $filtersForm->get('priceMax')->getData();
+        $offerTypes = $filtersForm->get('offerTypes')->getData();
+        return $offerService->findOffers(
+            $bookingOffer->getDepartureSpot(),
+            $bookingOffer->getDestination(),
+            $bookingOffer->getDepartureDate(),
+            $bookingOffer->getComebackDate(),
+            $priceMin,
+            $priceMax,
+            $offerTypes
+        );
+    }
+
+    private function getOffersBasedOnRequestQuery(BookingOfferService $offerService, Request $request, BookingOffer $bookingOffer)
+    {
+        $requestParams = $request->query->get('booking_offer_search');
+        $departureSpot = $requestParams['departureSpot'] ?? null;
+        $destination = $requestParams['destination'] ?? null;
+        $departureDate = $requestParams['departureDate'] ?? null;
+        $comebackDate = $requestParams['comebackDate'] ?? null;
+        if($departureDate!=null) {
+            $bookingOffer->setDepartureDate($this->getWellFormattedDate($departureDate));
+        }
+        if($comebackDate!=null) {
+            $bookingOffer->setComebackDate($this->getWellFormattedDate($comebackDate));
+        }
+        $bookingOffer->setDepartureSpot($departureSpot);
+        $bookingOffer->setDestination($this->getDoctrine()->getRepository(Destination::class)->find($destination));
+        return $offerService->findOffers($departureSpot,
+            $destination,
+            $bookingOffer->getDepartureDate(),
+            $bookingOffer->getComebackDate());
+    }
+
+    private function getOffersBasedOnOfferType(BookingOfferService $offerService, string $offerTypeName)
+    {
+        $offerType = $this->getDoctrine()->getRepository(BookingOfferType::class)->findOneBy(['typeName' => $offerTypeName]);
+        if($offerType!=null) {
+            $offers = $offerService->findOffers(null, null, null, null, null, null, [$offerType]);
+        } else {
+            $offers = null;
+        }
+        return $offers;
     }
 }
