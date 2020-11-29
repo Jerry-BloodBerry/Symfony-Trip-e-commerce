1bca43d6fc9b17320453d625ad54881d2fcf8008
martamazurkiewicz <marta@mazurkiewi.cz>
2020-11-29 14:51:03 +0100

    Added SettingController

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,8 +58,7 @@ class User implements UserInterface
 
     public function __construct()
     {
-        $this->customersRatings = new ArrayCollection();
-        $this->reservations = new ArrayCollection();
+
     }
 
     public function getId(): ?int
@@ -179,36 +178,6 @@ class User implements UserInterface
     public function setRegistrationDate(\DateTimeInterface $registrationDate): self
     {
         $this->registrationDate = $registrationDate;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Reservation[]
-     */
-    public function getReservations(): Collection
-    {
-        return $this->reservations;
-    }
-
-    public function addReservation(Reservation $reservation): self
-    {
-        if (!$this->reservations->contains($reservation)) {
-            $this->reservations[] = $reservation;
-            $reservation->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeReservation(Reservation $reservation): self
-    {
-        if ($this->reservations->removeElement($reservation)) {
-            // set the owning side to null (unless already changed)
-            if ($reservation->getUser() === $this) {
-                $reservation->setUser(null);
-            }
-        }
 
         return $this;
     }
