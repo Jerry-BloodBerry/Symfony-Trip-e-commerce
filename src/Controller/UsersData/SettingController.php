60d007c6cb6aeb90b566bab1991d07c408327b36
martamazurkiewicz <marta@mazurkiewi.cz>
2020-12-02 11:27:08 +0100

    draft of Edit Data page

--- src/Controller/UsersData/SettingController.php
+++ src/Controller/UsersData/SettingController.php
@@ -4,23 +4,46 @@
 namespace App\Controller\UsersData;
 
 
+use App\Form\SettingsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class SettingController extends AbstractController
 {
     /**
      * @Route("/settings", name="settings")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $auth_checker = $this->get('security.authorization_checker');
         if($auth_checker->isGranted('ROLE_USER')) {
-            $user = $this->get('security.token_storage')->getToken()->getUser();
+            $user = $this->getUser();
+            $name = $user->getFirstName();
+            $lastName = $user->getLastName();
+            $email = $user->getEmail();
+            $updatedUser = new User();
+            $settingsForm = $this->createForm(SettingsType::class, $updatedUser, [
+                'method' => 'GET'
+            ]);
+            $settingsForm->handleRequest($request);
+            if ($settingsForm->isSubmitted() && $settingsForm->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                //$em->persist($updatedUser);
+                //$em->flush();
+                return $this->redirectToRoute("home");
+            }
+
             return $this->render('settings/index.html.twig', [
                 'controller_name' => 'SettingController',
-                'user' => $user
+                'firstName' => $name,
+                'lastName' => $lastName,
+                'email' => $email,
+                'settingsForm' => $settingsForm
             ]);
         }
         else
